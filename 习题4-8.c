9b83867d7c05cfebc575ab3135431eb16fdaaa16
JJchen2018 <1054051924@qq.com>
2018-04-13 16:19:30 +0800

    Initial commit

---
+++ 习题4-8.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+int main()
+{
+	int chengJi;
+	printf("ÇëÊäÈë³É¼¨ ");
+	scanf("%d",&chengJi);
+
+	if(chengJi>=90)
+	{
+		printf("'A'");
+	}
+	else if(chengJi>=80)
+	{
+		printf("'B'");
+	}
+		else if(chengJi>=70)
+	{
+		printf("'C'");
+	}
+		else if(chengJi>=60)
+	{
+		printf("'D'");
+	}
+	else
+	{
+		printf("'E'");
+    }
+	return 0;
+}
